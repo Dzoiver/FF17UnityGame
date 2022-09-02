3f191c5c26b390c486e338ab65cb8894cf691a62
Bezrukov Kirill <doomseifer@mail.ru>
2022-09-02 16:57:00 +0400

    Block doors dialogues and block

---
+++ Assets/Scripts/Crypt/ArenaBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaBlock : MonoBehaviour, IUsableObjects
+{
+    [SerializeField] DialogueScriptable dialogue1;
+    [SerializeField] Sprite sprite;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    public void Action()
+    {
+        DialogueManager.instance.fillPlayDial(dialogue1.messages, false, sprite);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
